ac81a69765584adf23f9625bfbf8b7f97c8e6b9e
maxicecilia <maxicecilia@gmail.com>
2013-12-27 09:55:55 -0300

    Handle simple numbers as formula

--- dnd_tools/static/js/DiceRoller.js
+++ dnd_tools/static/js/DiceRoller.js
@@ -33,22 +33,22 @@
  *
  */
 var DiceRoller = function() {
- 
+
   // Private
- 
+
   // Create a "data type" to represent the roll results
   var ResultSet = function() {
     this.rolls = [];
     this.modifier = 0;
     this.total = 0;
   };
- 
+
   // Add a toString method for convenience
   ResultSet.prototype.toString = function() {
     var rolls = this.rolls.join(' + ');
     var modifier = this.modifier;
     var total = this.total;
- 
+
     if (modifier > 0) {
       return rolls + ' + ' + modifier + ' = ' + total;
     } else if (modifier < 0) {
@@ -57,26 +57,31 @@ var DiceRoller = function() {
       return (rolls == total) ? total : rolls + ' = ' + total;
     }
   };
- 
+
   /**
    * Parse formula into component parts
    * Returns object on success and null on failure
    */
   var parse = function(formula) {
+    /* If formula is just a number, then treat it as a modifier */
+    if (!isNaN(formula)) {
+      return { rolls: 0, sides: 0, modifier: parseInt(formula, 10) };
+    }
+
     var matches = formula.match(/^(\d+)?d(\d+)([+-]\d+)?$/i);
-    if (matches === null || matches[2] == 0) {
+    if (matches === null || matches[2] === 0) {
       return null;
     }
- 
+
     var rolls    = (matches[1] !== undefined) ? (matches[1] - 0) : 1;
     var sides    = (matches[2] !== undefined) ? (matches[2] - 0) : 0;
     var modifier = (matches[3] !== undefined) ? (matches[3] - 0) : 0;
- 
+
     return { rolls: rolls, sides: sides, modifier: modifier };
   };
- 
+
   // Public
- 
+
   /**
    * Roll the dice described in formula
    * Returns a ResultSet on success and null on failure
@@ -86,59 +91,26 @@ var DiceRoller = function() {
     if (pieces === null) {
       return null;
     }
- 
+
     var results = new ResultSet();
- 
+
     // rolls
     for (var i = 0; i < pieces.rolls; i++) {
       results.rolls[i] = (1 + Math.floor(Math.random() * pieces.sides));
     }
- 
+
     // modifier
     results.modifier = pieces.modifier;
- 
+
     // total
-    for (var i = 0; i < results.rolls.length; i++) {
+    for (i = 0; i < results.rolls.length; i++) {
       results.total += results.rolls[i];
     }
     results.total += pieces.modifier;
- 
-    return results.total;
+
+    return results;
   };
 
-   /**
-   * Roll the dice described in formula
-   * Returns a ResultSet on success and null on failure
-   * Discarding the lowest throw of the lot
-   * Not supported on IE 8 and below
-   */
-  this.rollPJ = function(formula) {
-    var pieces = parse(formula);
-    if (pieces === null) {
-      return null;
-    }
- 
-    var results = new ResultSet();
- 
-    // rolls
-
-    for (var i = 0; i < pieces.rolls; i++) {
-      results.rolls[i] = (1 + Math.floor(Math.random() * pieces.sides));
-    }
- 
-    // modifier
-    results.modifier = pieces.modifier;
- 
-    // total
-    for (var i = 0; i < results.rolls.length; i++) {
-      if (i != results.rolls.indexOf(Math.min.apply(null, results.rolls)) ) {
-        results.total += results.rolls[i];
-      }
-    }
-    results.total += pieces.modifier;
-    return results.total;
-  };
- 
   /**
    * Validates the format of formula
    * Returns true on success and false on failure
@@ -146,5 +118,5 @@ var DiceRoller = function() {
   this.validate = function(formula) {
     return (parse(formula) === null) ? false : true ;
   };
- 
+
 };
